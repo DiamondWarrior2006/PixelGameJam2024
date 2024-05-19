1a97b426a1cd6c309ce0a2c30f40e140449311f4
JohnElliottComp <JohnElliottComp@gmail.com>
2024-05-19 15:40:51 +0100

    Added and Changed: Added ST_Soft, Alert and Active, which can now be looped seamlesly, and changed music_player.gd to import those files

--- Scripts/music_player.gd
+++ Scripts/music_player.gd
@@ -1,11 +1,17 @@
 extends AudioStreamPlayer
 
 
-@onready var normal_theme = preload("res://Music/ST_SoftExplore1.mp3")
-@onready var active_theme = preload("res://Music/ST_ActiveLayerWIP1.mp3")
+@onready var normal_theme = preload("res://Music/ST_Soft.wav")
+@onready var alert_theme = preload("res://Music/ST_Alert.wav")
+@onready var active_theme = preload("res://Music/ST_Active.wav")
 
 func play_normal_theme():
 	stream = normal_theme
+	volume_db = -10
+	play()
+
+func play_alert_theme():
+	stream = alert_theme
 	volume_db = -10
 	play()
 
@@ -13,5 +19,3 @@ func play_active_theme():
 	stream = active_theme
 	volume_db = -10
 	play()
-
-
